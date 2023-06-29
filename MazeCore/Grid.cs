e6a215138aa42974fa5fec7ba3d13d24ddbdfb1a
johan idstam <johan@idstam.com>
2023-06-29 18:33:47 +0200

    Initiating grid and tests

--- MazeCore/Grid.cs
+++ MazeCore/Grid.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MazeCore
 {
@@ -22,19 +19,67 @@ namespace MazeCore
         {
             var ret = new Cell[_columns][];
 
-            for(int c = 0; c < _columns; c++)
+            for (int c = 0; c < _columns; c++)
             {
                 ret[c] = new Cell[_rows];
             }
+            for (int c = 0; c < _columns; ++c)
+            {
+                for (int r = 0; r < _rows; ++r)
+                {
+                    ret[c][r] = new Cell(r, c);
+                }
+            }
             return ret;
         }
+
         private void ConfigureCells()
         {
-
+            foreach(var cell in Cells)
+            {
+                cell.North = GetCell(cell.Row - 1, cell.Column);
+                cell.South= GetCell(cell.Row +1, cell.Column);
+                cell.East= GetCell(cell.Row, cell.Column+1);
+                cell.West = GetCell(cell.Row, cell.Column - 1);
+            }
         }
         public Cell? GetCell(int row, int column)
         {
-            return _grid[row][column];
+            if (row < 0 || column < 0 || row >= _rows - 1 || column >= _columns - 1)
+            {
+                return null;
+            }
+            return _grid[column][row];
         }
+        public Cell GetRandom()
+        {
+            var rnd = new Random();
+            var r = rnd.Next(0, _rows);
+            var c = rnd.Next(0, _columns);
+            var ret = GetCell(r, c);
+
+            if( ret != null)
+            {
+                return ret;
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+        public IEnumerable<Cell> Cells
+        {
+            get
+            {
+                for(int c = 0; c < _columns; ++c)
+                {
+                    for(int r = 0; r < _rows; ++r)
+                    {
+                        yield return _grid[c][r];
+                    }
+                }
+            }
+        }
+        public int Size { get => _rows * _columns; } 
     }
 }
--- MazeTest/GridTest.cs
+++ MazeTest/GridTest.cs
@@ -12,5 +12,26 @@ namespace MazeTest
             Assert.IsNotNull(grid);
             Assert.IsNull(grid.GetCell(5, 5));
         }
+        [TestMethod]
+        public void TestNeigbours()
+        {
+            var grid = new MazeCore.Grid(3, 3);
+            Assert.IsNull(grid.GetCell(0, 0)?.North);
+            Assert.IsNotNull(grid.GetCell(0, 0)?.South);
+            Assert.IsNotNull(grid.GetCell(0, 0)?.East);
+            Assert.IsNull(grid.GetCell(0, 0)?.West);
+        }
+        [TestMethod]
+        public void SizeTest()
+        {
+            var grid = new MazeCore.Grid(3, 3);
+            Assert.AreEqual(9, grid.Size);
+
+            grid = new MazeCore.Grid(5, 3);
+            Assert.AreEqual(15, grid.Size);
+
+            grid = new MazeCore.Grid(3, 6);
+            Assert.AreEqual(18, grid.Size);
+        }
     }
 }
